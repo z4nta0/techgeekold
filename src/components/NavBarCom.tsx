ef2d3ed3384a8b26336f63789d6309c593845bd8
z4nta0 <z4nta0@gmail.com>
2025-11-14 12:19:12 -0600

    Add some useStates to the navbar componet in ./src/components/NavBarCom.tsx

--- src/components/NavBarCom.tsx
+++ src/components/NavBarCom.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import reactLogo from '../assets/react.svg';
 
 interface NavBarComProps {
@@ -7,43 +7,102 @@ interface NavBarComProps {
 
 };
 
+/*type ClickCountObj = {
+
+  logoCount: number;
+  homeCount: number;
+
+};*/
+
 export const NavBarCom : React.FC<NavBarComProps> = (props) => {
 
-  const handleClick1 = () => {
+  const clickCountObj : Record<string, number> = {
 
-    alert(`You clicked on the logo link for the ${props.name} Home page!`);
-
-  };
-
-  const handleClick2 = () => {
-
-    alert(`You clicked on the nav link for the ${props.name} Home page!`);
+    logoCount: 0,
+    homeCount: 0,
 
   };
 
-  const handleClick3 = () => {
+  const [ curCouObj, setCount1 ] = useState(clickCountObj);
 
-    alert(`You clicked on the nav link for the ${props.name} About page!`);
+  const handleClick1 = ( { currentTarget } : React.MouseEvent<HTMLAnchorElement> ) => {
+
+    if (currentTarget.hasAttribute('data-clickname')) {
+
+      const clickname = currentTarget.getAttribute('data-clickname') as keyof typeof clickCountObj;
+
+      setCount1 (() => {
+
+        const prevCount : number = curCouObj[clickname];
+        const newCount : number = prevCount + 1;
+
+        return (
+          { ...curCouObj, [clickname]: newCount }
+        );
+
+      });
+
+      const linkName = currentTarget.getAttribute('data-linkname');
+
+      alert(`You clicked on the ${linkName} link for the ${props.name} Home page ${curCouObj[clickname] + 1} times!`);
+
+    };
 
   };
 
-  const handleClick4 = () => {
+  const clickCountArr : number[] = [ 0, 0 ];
 
-    alert(`You clicked on the nav link for the ${props.name} Contact page!`);
+  const [ curCouArr, setCount ] = useState(clickCountArr);
+
+  const handleClick2 = ( { currentTarget } : React.MouseEvent<HTMLAnchorElement> ) => {
+
+    if (currentTarget.hasAttribute('data-clickindex')) {
+
+      const clickIndexStr = currentTarget.getAttribute('data-clickindex') as string;
+      const clickIndexNum = Number(clickIndexStr);
+
+      setCount (() => {
+
+        const prevCount : number = curCouArr[clickIndexNum];
+        const newCount : number = prevCount + 1;
+
+        if (clickIndexNum === 0) {
+
+          return (
+            [ newCount, curCouArr[1] ]
+          );
+
+        }
+
+        else {
+
+          return (
+            [ curCouArr[0], newCount ]
+          );
+
+        };
+
+      });
+
+      const linkName = currentTarget.getAttribute('data-linkname');
+
+      alert(`You clicked on the navbar link for the ${props.name} ${linkName} page ${curCouArr[clickIndexNum] + 1} times!`);
+
+    };
 
   };
 
   return (
 
     <nav className='nav-bar-sty' style={{  display: 'flex', padding: '1rem', backgroundColor: '#282c34', color: 'white' }}>
-      <a onClick={handleClick1} href="#home" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around', color: 'white', textDecoration: 'none' }}>
+      <a onClick={handleClick1} href="#home" data-linkname="logo" data-clickname="logoCount" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around', color: 'white', textDecoration: 'none' }}>
         <img src={reactLogo} className="nav-log-sty" alt="React logo" />
         <p style={{ paddingLeft: '1rem' }}>Tech Geek</p>
       </a>
       <ul style={{ listStyle: 'none', display: 'flex', gap: '1rem' }}>
-        <li><a onClick={handleClick2} href="#home" style={{ color: 'white', textDecoration: 'none' }}>Home</a></li>
-        <li><a onClick={handleClick3} href="#about" style={{ color: 'white', textDecoration: 'none' }}>About</a></li>
-        <li><a onClick={handleClick4} href="#contact" style={{ color: 'white', textDecoration: 'none' }}>Contact</a></li>
+        <li><a onClick={handleClick1} data-linkname="navbar" data-clickname="homeCount" href="#home" style={{ color: 'white', textDecoration: 'none' }}>Home</a></li>
+        <li><a onClick={handleClick2} data-linkname="About" data-clickindex="0" href="#about" style={{ color: 'white', textDecoration: 'none' }}>About</a></li>
+        <li><a onClick={handleClick2} data-linkname="Contact" data-clickindex="1" href="#contact" style={{ color: 'white', textDecoration: 'none' }}>Contact</a></li>
       </ul>
     </nav>
 
