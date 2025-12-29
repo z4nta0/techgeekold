38ea6546589d0f457c6dedd0a53d0661b78261e6
z4nta0 <z4nta0@gmail.com>
2025-12-29 11:43:46 -0600

    Release version 0.36.2 Comment and fomart src/utilities/ranNumFun.tsx

--- src/utilities/ranNumFun.tsx
+++ src/utilities/ranNumFun.tsx
@@ -1,39 +1,76 @@
+
 // #region ranNumFun
 
 /**
- ** This will generate a random number between the provided minimum and maximum values.
- ** 
- ** @param maxNum = The maximum value of the random number
- ** @param minNum = The minimum value of the random number
- ** @returns      = The generated random number
- **
- **/
+ * hasKeyFun = Has Key Function
+ *
+ * @summary
+ * This custom function will generate a random number between the provided
+ * minimum and maximum values. This is inclusive of both the minimum and maximum
+ * values. It utilizes JavaScript's built in Math.random() function to generate
+ * the random number, and Math.round() to round the result to the nearest whole
+ * number.
+ *
+ * @author z4ntao <https://github.com/z4nta0>
+ * 
+ * @param maxNum - The maximum value of the random number.
+ * @param minNum - The minimum value of the random number.
+ * 
+ * @returns A random number between, and including, the minimum and maximum values.
+ * 
+ * @example
+ * ```ts
+ * ranNumFun( { maxNum : 10, minNum : 1 } ) // returns a random number between 1 and 10, inclusive.
+ * ```
+ *
+*/
 
-// #region Type Declarations
 
-// random number function parameters = parameters provided to this function when called
+// #region Function Type Declarations
+
+
+/**
+ * RanNumFunPar = Random Number Function Parameter will store the maximum and minimum number constraints (inclusive) for generating a random number.
+ *
+ * @property maxNum = Maximum Number will store the number for establishing the maximum value constraint for the random number, inclusive.
+ * @property minNum = Minimum Number will store the number for establishing the minimum value constraint for the random number, inclusive.
+ *
+*/
+
 type RanNumFunPar = {
 
-    maxNum : number; // maximum number = number for establishing the maximum value of the random number, exclusive
-    minNum : number; // minimum number = number for establishing the minimum value of the random number, inclusive
+    maxNum : number;
+    minNum : number;
 
 };
 
-type RanNumFun    = ( ranNumFunPar : RanNumFunPar ) => RanNumFunRet; // random number function        = generate and return a random number using the provided parameters
-type RanNumFunRet = number;                                          // random number function return = number generated randomly between a minimum and maximum value
+/** Random Number Function = This function will generate and return a random number using the provided parameters as its constraints. */
+type RanNumFun             = ( ranNumFunPar : RanNumFunPar ) => number;
 
-// #endregion Type Declarations
+
+// #endregion Function Type Declarations
 
 
 
 // #region Function Body
 
-export const ranNumFun : RanNumFun = ( { maxNum, minNum } ) => {
+const ranNumFun : RanNumFun = ( { maxNum, minNum } ) => {
 
+    /** The Math.round() function is a Javascript static method that returns the value of a number rounded to the nearest integer. Numbers with a fractional part of 0.5 or greater are rounded up, and numbers less than 0.5 are rounded down. */
+    /** The Math.random() function is Javascript static method that generates a pseudo random floating point number that is greater than or equal to 0 and less than 1 (inclusive of 0, but exclusive of 1). */
+    /** minNum is added at the beginning so that even the smallest random number that is generated will evaluate to at least minNum (example, 0.001 will be added to minNum and then rounded down to minNum). */
+    /** The expression ( maxNum - minNum ) is multiplied to Math.random() so that it scales the random number that is generated (it will always a floating point number between 0 and 1) to the desired range. */
     return Math.round( minNum  + Math.random() * ( maxNum - minNum ) );
 
 };
 
 // #endregion Function Body
 
+
 // #endregion ranNumFun
+
+
+
+export default ranNumFun;
+
+
